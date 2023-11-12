85c1b9fd969e27bb48b836a333ba30aca0fad8ab
9insane9 <145199591+9insane9@users.noreply.github.com>
2023-11-12 14:53:03 +0200

    banan

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let result;
+
+    if (typeof a === 'number'           
+        && typeof b === 'number'
+        && !(a < 0) 
+        && !(b < 0) ){
+
+            if (a < b) {                          
+                for (a ; a <= b ; a++) {    
+                    result += a;
+                }
+                return result; 
+
+            } else if (a > b) {
+                for (b ; b <= a ; b++) {
+                    result += b;
+                }
+                return result;
+            }            
+    } else {
+        return ('ERROR');
+    }
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
